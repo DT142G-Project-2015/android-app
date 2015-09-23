7f62941fe9a178a5eb3aa109f9002908c70c10b7
Rafid <rama1300@student.miun.se>
2015-09-23 17:48:44 +0200

    added working exit button to the option

--- app/src/main/java/com/example/proxymeister/antonsskafferi/MainActivity.java
+++ app/src/main/java/com/example/proxymeister/antonsskafferi/MainActivity.java
@@ -28,7 +28,12 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.actionbar_settings) {
+            return true;
+        }
+
+        if (id == R.id.actionbar_exit) {
+            finish();
             return true;
         }
 
