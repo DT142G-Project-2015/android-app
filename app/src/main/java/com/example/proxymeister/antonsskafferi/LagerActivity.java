dfab320908feb6850d47856b7a0d5cafaf387720
Pontus <poto1300@student.miun.se>
2015-09-28 23:52:11 +0200

    Added a demo entry to Lager

--- app/src/main/java/com/example/proxymeister/antonsskafferi/LagerActivity.java
+++ app/src/main/java/com/example/proxymeister/antonsskafferi/LagerActivity.java
@@ -5,14 +5,42 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.List;
 
-public class LagerActivity extends AppCompatActivity {
+public class LagerActivity extends AppCompatActivity  {
+    private List<String> lager_entry;
+    private ListView lager_list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lager);
+        lager_list = (ListView)findViewById(R.id.lagerList);
+
+        lager_entry = new ArrayList<>();
+        {
+            lager_entry.add("Renkött    |   6 (kg)    |    15 dagar ");
+            lager_entry.add("Drakfilé    | 10 (gram) |  31708 dagar ");
+            lager_entry.add("Revbenspjäll    |   17 (kg)    |   5 dagar  ");
+        }
+
+
+        /* used with URL-fetch
+        for (Item p : entries) {
+            strings.add(p.toString());
+        }
+        */
+
+        // stores entries
+        ArrayAdapter<String> entry_keeper =
+                new ArrayAdapter<>(LagerActivity.this, android.R.layout.simple_list_item_1, lager_entry);
+
+        // pass entries to the ListView
+        lager_list.setAdapter(entry_keeper);
     }
 
     @Override
