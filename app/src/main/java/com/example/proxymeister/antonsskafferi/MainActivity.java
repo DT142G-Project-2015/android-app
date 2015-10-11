7f238c8993cee8dca09ebe9a88b3f32080913acb
Rafid <rama1300@student.miun.se>
2015-10-11 03:33:45 +0200

    quick but important changes to popup function: properly terminates, inhibits to 1 thread, more efficient, does not crash when no meals are readyToServe, syncs with mainthread by waiting. note: popups limited to MainActivity, can be expanded.

--- app/src/main/java/com/example/proxymeister/antonsskafferi/MainActivity.java
+++ app/src/main/java/com/example/proxymeister/antonsskafferi/MainActivity.java
@@ -23,6 +23,8 @@ import retrofit.Retrofit;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private static boolean threadNotRunning = true;
     private int dflag = 0;
     private Thread client;
 
@@ -30,10 +32,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // TODO: consider making the popup global, pro: you cant miss it, con: maybe annoying
-        // TODO: if( account is logged in as waiter, start thread)
+        // TODO: thread global, pro: you cant miss it, con: disruptive
+        // TODO: get account info. if account is waiter, create thread.
         client = new Thread(new CheckForOrders());
-        client.start();
+
+        if (threadNotRunning) {
+            client.start();
+        }
 
         //Gets current screen orientation value
         int screen = getResources().getConfiguration().orientation;
@@ -54,31 +59,26 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void showDialog(final String id, final String booth)
-    {
+    public void showDialog(final String id, final String booth) {
+        // necessary context change, UI CANNOT be handled in the new thread
         runOnUiThread(new Runnable() {
             public void run() {
-                // necessary context change, UI CANNOT be handled in the new thread
-                runOnUiThread(new Runnable() {
-                    public void run() {
-                        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                        builder.setTitle("Food is ready!");
-                        builder.setMessage("Order ID: " + id + " /  Booth: " + booth);
-                        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                                dialog.dismiss();
-                                dflag = 1;
-                            }
-                        });
-                        AlertDialog dialog = builder.create();
-                        dialog.show();
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Food is ready!");
+                builder.setMessage("Order ID: " + id + " /  Booth: " + booth);
+                builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.dismiss();
+                        dflag = 1;
                     }
                 });
+                AlertDialog dialog = builder.create();
+                dialog.show();
             }
         });
     }
 
-
+    //TODO: use the change status API to clear the "readyToServe", so that it does not repeat notifications
     // always listen for a message. if a dish is done, display dialog.
     class CheckForOrders implements Runnable {
 
@@ -87,119 +87,154 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void run() {
 
+            threadNotRunning = false;
+
             while (true) {
                 try {
                     Call<List<Order>> call = Utils.getApi().getOrdersByStatus("readyToServe");
-                    if (call != null) {
-                        call.enqueue(new Callback<List<Order>>() {
-                            @Override
-                            public void onResponse(Response<List<Order>> response, Retrofit retrofit) {
+                    call.enqueue(new Callback<List<Order>>() {
+                        @Override
+                        public void onResponse(Response<List<Order>> response, Retrofit retrofit) {
 
-                                int statusCode = response.code();
-                                Log.i(MainActivity.class.getName(), "Status: " + statusCode);
+                            int statusCode = response.code();
+                            Log.i(MainActivity.class.getName(), "Status: " + statusCode);
 
-                                readyOrder = response.body();
+                            readyOrder = response.body();
+
+                            if (!(readyOrder.isEmpty() || readyOrder == null)) {
+                                dflag = 2;
+                                showDialog(Integer.toString(readyOrder.get(0).getId()), Integer.toString(readyOrder.get(0).getBooth()));
+                            } else {
+                                System.out.println("cc");
+                                dflag = 3;
                             }
-
-                            @Override
-                            public void onFailure(Throwable t) {
-                                Log.i(MainActivity.class.getName(), "Failed to fetch data: " + t.getMessage());
-                            }
-                        });
-
-                        showDialog(Integer.toString(readyOrder.get(0).getId()), Integer.toString(readyOrder.get(0).getBooth()));
-
-                        while (dflag != 1) {
-                            Thread.sleep(5000);
                         }
-                        dflag = 0;
 
-                        //TODO: use the change status API here to clear the "readyToServe", so that it does not repeat notifications
+                        @Override
+                        public void onFailure(Throwable t) {
+                            Log.i(MainActivity.class.getName(), "Failed to fetch data: " + t.getMessage());
+                        }
+                    });
+
+                    // waiting for uithread to catch up
+                    while (dflag == 0)
+                    {
+                        Thread.sleep(100);
                     }
-                } catch (Exception e) {
+
+                    // nothing found, continue listening
+                    if (dflag == 3)
+                    {
+                        Thread.sleep(2000);
+                    }
+                    else if (dflag == 2)
+                    {
+                        while (dflag != 1)
+                        {
+                            Thread.sleep(3000);
+                        }
+                    }
+
+                    dflag = 0;
+
+                } catch (InterruptedException e) {
                     e.printStackTrace();
+                    threadNotRunning = true;
+                    return;
                 }
             }
         }
     }
 
 
-        @Override
-        public void onConfigurationChanged(Configuration newConfig) {
-            super.onConfigurationChanged(newConfig);
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
 
-            // Checks the orientation of the screen
-            if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
-                setContentView(R.layout.activity_main_land);
-            } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
-                setContentView(R.layout.activity_main);
-            }
+        // Checks the orientation of the screen
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            setContentView(R.layout.activity_main_land);
+        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            setContentView(R.layout.activity_main);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if(id == android.R.id.home)
+        {
+            client.interrupt();
         }
 
-        @Override
-        public boolean onCreateOptionsMenu(Menu menu) {
-            // Inflate the menu; this adds items to the action bar if it is present.
-            getMenuInflater().inflate(R.menu.menu_main, menu);
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.actionbar_settings) {
+            return true;
+
+        }
+
+        if (id == R.id.actionbar_exit) {
+            finish();
             return true;
         }
 
-        @Override
-        public boolean onOptionsItemSelected(MenuItem item) {
-            // Handle action bar item clicks here. The action bar will
-            // automatically handle clicks on the Home/Up button, so long
-            // as you specify a parent activity in AndroidManifest.xml.
-            int id = item.getItemId();
-
-            //noinspection SimplifiableIfStatement
-            if (id == R.id.actionbar_settings) {
-                new Thread(new CheckForOrders()).start();
-                return true;
-
-            }
-
-            if (id == R.id.actionbar_exit) {
-                finish();
-                return true;
-            }
-
-            return super.onOptionsItemSelected(item);
-
-        }
-
-        // Called on by the Lager button in activity_main.xml
-        // Starts the lager activity
-        public void startLagerActivity(View view) {
-            Intent intent;
-            intent = new Intent(MainActivity.this, LagerActivity.class);
-            startActivity(intent);
-        }
-
-        //-------------------------------------------------------------------------------------//
-        // When you click on any of the buttons "beställningar/kök/meny/lager", the button is sent in as
-        // an argument of type "view" to these functions. When the corresponding function runs, it sets
-        // up a new activity called "OrderActivity". Navigate to that file if you want to implement
-        // something related.
-
-        public void openOrderActivity(View view) {
-            Intent intent = new Intent(this, OrderActivity.class);
-            startActivity(intent);
-        }
-
-        public void openKitchenActivity(View view) {
-            Intent intent = new Intent(this, KitchenActivity.class);
-            startActivity(intent);
-        }
-
-        public void openMenuActivity(View view) {
-            Intent intent = new Intent(this, MenuListActivity.class);
-            startActivity(intent);
-        }
-
-        //public void MenuListActivity(View view) {
-        //  Intent intent = new Intent(this, MenuActivity.class);
-        //startActivity(intent);
-        //}
-        //-------------------------------------------------------------------------------------//
-
+        return super.onOptionsItemSelected(item);
 
     }
+
+    // Called on by the Lager button in activity_main.xml
+    // Starts the lager activity
+    public void startLagerActivity(View view) {
+        client.interrupt();
+        Intent intent = new Intent(MainActivity.this, LagerActivity.class);
+        startActivity(intent);
+    }
+
+    //-------------------------------------------------------------------------------------//
+    // When you click on any of the buttons "beställningar/kök/meny/lager", the button is sent in as
+    // an argument of type "view" to these functions. When the corresponding function runs, it sets
+    // up a new activity called "OrderActivity". Navigate to that file if you want to implement
+    // something related.
+
+    public void openOrderActivity(View view) {
+        client.interrupt();
+        Intent intent = new Intent(this, OrderActivity.class);
+        startActivity(intent);
+    }
+
+    public void openKitchenActivity(View view) {
+        client.interrupt();
+        Intent intent = new Intent(this, KitchenActivity.class);
+        startActivity(intent);
+    }
+
+    public void openMenuActivity(View view) {
+        client.interrupt();
+        Intent intent = new Intent(this, MenuListActivity.class);
+        startActivity(intent);
+    }
+
+    //public void MenuListActivity(View view) {
+    //  Intent intent = new Intent(this, MenuActivity.class);
+    //startActivity(intent);
+    //}
+    //-------------------------------------------------------------------------------------//
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        System.out.println("okk");
+        client.interrupt();
+    }
+}
