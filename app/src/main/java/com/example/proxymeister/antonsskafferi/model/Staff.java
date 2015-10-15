d44df5d24a53e279944867633cc4021b3db50b3f
Gergi1 <gema1300@student.miun.se>
2015-10-15 16:10:59 +0200

    have fixed the login and it has now a 5 times test before it refuses your entry. password/username are the same

---
+++ app/src/main/java/com/example/proxymeister/antonsskafferi/model/Staff.java
@@ -0,0 +1,10 @@
+package com.example.proxymeister.antonsskafferi.model;
+
+
+public class Staff {
+    public int id;
+    public String username;
+    public int role;
+    public String first_name;
+    public String last_name;
+}
